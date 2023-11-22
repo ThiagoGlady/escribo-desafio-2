11e4dc495c90eadde6015f2170aa63793f2b5fbe
ThiagoGlady <thiagoglady@gmail.com>
2023-11-22 11:12:28 -0300

    refactor: :zap: modificações para que os dados para conexão com o banco de dados fosse feito atráveis do .env

--- processo/src/model/connection.js
+++ processo/src/model/connection.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
+
 const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'escribo'
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
 });
 
 module.exports = connection;
